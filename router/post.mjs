143efc621f37d7532b69831fdcb9e3249420a549
whitepanguin <dun04045@gmail.com>
2025-04-24 12:19:26 +0900

    posts done

--- router/post.mjs
+++ router/post.mjs
@@ -1,5 +1,6 @@
 // 글 관련 작동
 import express from "express";
+import * as postController from "../controller/post.mjs";
 
 const router = express.Router();
 
@@ -8,23 +9,28 @@ const router = express.Router();
 // GET
 // http://127.0.0.1:8080/posts/
 // http://127.0.0.1:8080/posts?userid=apple
+router.get("/", postController.getPosts);
 
 // 글번호에 대한 포스트 가져오기
 // GET
 // http://127.0.0.1:8080/posts/:id
+router.get("/:id", postController.getPostId);
 
 // 포스트 쓰기
 // POST
 // http://127.0.0.1:8080/posts
 // json 형태로 입력 후 저장
+router.post("/", postController.createPost);
 
 // 포스트 수정
 // PUT
 // http://127.0.0.1:8080/posts/:id
 // json 형태로 입력 후 저장
+router.put("/:id", postController.updatePost);
 
 // 포스트 삭제
 // DELETE
 // http://127.0.0.1:8080/posts/:id
+router.delete("/:id", postController.deletePost);
 
 export default router;
